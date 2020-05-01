49d039f3b90738a4a817b8082a0670a808fd22a4
gabil <maria.g984@hotmail.com>
2020-05-01 18:33:29 -0500

    commit mamappv1

---
+++ Mamapp/Assets/Scripts/UserData.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserData : MonoBehaviour
+{
+    private string getUrlEscribir = "http://tadeolabhack.com:8081/test/Datos/mamappdb/escribirUsuario.php"; //http://soymariaojeda.com/
+
+    private string nombre = "";
+    private string correo = "";
+    private string contrasena = "";
+
+    public InputField campoNombre;
+    public InputField campoCorreo;
+    public InputField campoContrasena;
+
+    [SerializeField] private GameObject exitoso;
+    [SerializeField] private GameObject mensaje;
+
+    public void senData()
+    {
+        StartCoroutine(enviarDatosUsuario()); 
+    }
+
+    private IEnumerator enviarDatosUsuario()
+    {
+        nombre = campoNombre.text;
+        correo = campoCorreo.text;
+        contrasena = campoContrasena.text;  
+
+        print(nombre + " " + correo + " " + contrasena);
+
+        if (nombre == "" || correo == "" || contrasena == "") // Los int == 0
+        {
+            mensaje.SetActive(true);
+        }
+
+        else{
+
+            WWWForm formU = new WWWForm();
+            formU.AddField("nombre", nombre); // Primero como le puse en el request [], luego como se llama en la tabla
+            formU.AddField("correo", correo);
+            formU.AddField("contrasena", contrasena);
+
+            WWW retroalimentacion = new WWW(getUrlEscribir, formU);
+            yield return retroalimentacion;
+
+            print(retroalimentacion.text);
+
+            if (retroalimentacion.text == "Usuario agregado")
+            {
+                exitoso.SetActive(true);
+            }
+            else
+            {
+                exitoso.SetActive(true);
+            }
+        }
+
+       
+        
+    }
+}
